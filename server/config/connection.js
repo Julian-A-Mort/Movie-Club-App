c65b2ee5e2f74f970f1922e3eacc3fe5b7a7f52f
Julian <julian@wheresnick.com.au>
2024-01-27 13:01:33 +1100

    added env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
 // add website
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dbname', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+// mongoose.connection.on('error', err => {
+//     console.error(`MongoDB connection error: ${err}`);
+//     process.exit(1); // Optional: Exit the application on database connection error
+// });
 
 module.exports = mongoose.connection;
